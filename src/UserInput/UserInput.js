45478a11c03e4d0f9e9321fcb1d2be70fdac3bf2
Vincent Chen <vince_1728@hotmail.com>
2020-08-04 18:41:02 -0700

    export

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const userInput = () => {
     return (
         <div>
-
+            <input type="text"/>
         </div>
     );
 }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const userInput = () => {
+const userOutput = (props) => {
     return (
         <div>
-
+            <p>{props.username}</p>
+            <p>This is Line 1.</p>
+            <p>This is Line 2.</p>
         </div>
     );
 }
 
-export default userInput
+export default userOutput
